9e14c9f05e99362e0e2a21271d4b84ca446f16e3
githdavro <74291962+githdavro@users.noreply.github.com>
2023-11-29 22:16:14 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,8 @@
 <?php
+$con = mysqli_connect('localhost', 'root' , '' , 'datapeserta');
 
-$koneksi = mysqli_connect("localhost","root","","datapeserta")
-
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
 ?>
