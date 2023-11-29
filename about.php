9de242326930abc2123c4e47c85aeec1fdd84080
githdavro <74291962+githdavro@users.noreply.github.com>
2023-11-30 00:55:37 +0700

    Update about.php
    updated UI Layout

--- about.php
+++ about.php
@@ -1,115 +1,73 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About Page</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <style>
-        body {
-            font-family: 'Arial', sans-serif;
-            text-align: center;
-            padding: 20px;
-            background-color: #f4f4f4;
-            color: #333;
-        }
-
-        h2, h3 {
-            color: #333;
-            text-align: left;
-        }
-
-        p {
-            margin-bottom: 20px;
-            line-height: 1.6;
-            text-align: left;
-        }
-
-        a1 {
-            text-decoration: none;
-            background-color: #333;
-            color: #fff;
-            padding: 10px 20px;
-            border-radius: 5px;
-            transition: background-color 0.3s;
-            text-align: left;
-            display: inline-block;
-        }
-
-        a1:hover {
-            background-color: #555;
-        }
-
-        .container {
-            max-width: 800px;
-            margin: 0 auto;
-            background-color: #fff;
-            padding: 20px;
-            border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            text-align: left;
-        }
-
-        .social-icons {
-            margin-top: 20px;
-        }
-
-        .social-icons a {
-            margin-right: 10px;
-            color: #333;
-            font-size: 24px;
-        }
-
-        .copyright {
-            margin-top: 20px;
-            font-size: 14px;
-        }
-    </style>
+    <title>GitDaVro | About</title>
+    <link rel="stylesheet" href="style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <body>
+    <header class="header">
+        <a href="#" class="logosini">GitDaVro.</a>
 
-<div class="container">
+        <nav class="navbar">
+            <a href="home.php" >Home</a>
+            <a href="about.php" class="active">About</a>
+            <a href="projects.php">Projects</a>
+            <a href="contact.php">Contact</a>
+            <a href="?logout=true">Logout</a>
+        </nav>
+    </header>
 
-    <h2>About Githriff Ahmad Nibross Vroticca</h2>
 
-    <p>
-        I'm a passionate learner who's always willing to learn and work across technologies and domains.
-    </p>
+    <section class="about">
+    <div class="about-content">
+            <h1>About</h1>
+            <h3>Githriff Ahmad Nibross Vroticca</h3>
+            <p> 
+                I'm a passionate learner who's always willing to learn and work across technologies and domains.
+            </p>
+            <h3>Education</h3>
+            <p> 
+                <strong>MUHAMMADIYAH GRESIK UNIVERSITY</strong> (2022 - Now)<br>
+                <strong>SMK SEMEN GRESIK</strong> (2018 - 2021)<br>
+                <strong>SMPN 2 GRESIK</strong> (2015 - 2018)
+            </p>
+            <h3>Skills</h3>
+            <p> 
+            Digital drawings (ibisPaint), Photoshop, CorelDraw, Illustrator, C++ (Learning as Modding The Sims 3 UI Project), Javascript, HTML5, CSS3, jQuery (Learning),
+            PHP (Learning), Vue Js (Learning), ReactJs (Insyaallah learning), MySQL. Git, Bootstrap, Github,
+            VSCode, Linux (Learning to understand the ecosystem and the FOSS).
+            </p>
+        </div>
 
-    <h3>Education</h3>
-    <p>
-        <strong>MUHAMMADIYAH GRESIK UNIVERSITY</strong> (2022 - Now)<br>
-        <strong>SMK SEMEN GRESIK</strong> (2018 - 2021)<br>
-        <strong>SMPN 2 GRESIK</strong> (2015 - 2018)
-    </p>
+        <div class="home-inf"><p>Any suggestion would be appreciated. You can mail me at: <br>
+            <a href="mailto:githriffgresik@gmail.com" class="active">githriffgresik@gmail.com</a>
+        </div>
 
-    <h3>Skills</h3>
-    <p>
-        C++ (Learning as Modding The Sims 3 UI Project), Javascript, HTML5, CSS3, jQuery (Learning),
-        PHP (Learning), Vue Js (Learning), ReactJs (Insyaallah learning), MySQL. Git, Bootstrap, Github,
-        VSCode, Linux (Learning to understand the ecosystem and the FOSS).
-    </p>
+            <div class="home-sci">
+                <h3>Follow me:</h3><br>
+                <a href="https://github.com/githdavro/"><i class='bx bxl-github'></i></a>
+                <a href="https://www.facebook.com/githriff.nibross/"><i class='bx bxl-facebook'></i></a>
+                <a href="https://www.instagram.com/gith.amd_"><i class='bx bxl-instagram'></i></a>
+                <a href="#"><i class='bx bxl-whatsapp'></i></a>
+                <a href="https://t.me/s/Fre1z4"><i class='bx bxl-telegram'></i></a>
+            </div>
+            <div class="clock">
+                <h1 id="current-time"9></h1>
+            </div>
 
-    <!-- Tombol untuk kembali ke halaman home.php -->
-    <a href="home.php">Back to Home</a>
+            <span class="home-imgHover"></span>
+
+            <div class="home-bott">
+        <p>©2023 GitDaVro All Rights reserved. This site is made with kang love by <a href="https://instagram.com/gith.amd_" target="_blank" class="active">@gith.amd_</a></p>
+        </div>
+    </section>
+
 
     
-</div>
-
-<p>Any suggestion would be appreciated. You can mail me at <br>
-    <a href="mailto:githriffgresik@gmail.com">githriffgresik@gmail.com</a></p>
-
-    <div class="social-icons">
-        <span>Follow me:</span><br>
-        <a href="https://github.com/githdavro" target="_blank"><i class="fab fa-github"></i></a>
-        <a href="https://facebook.com/githriff" target="_blank"><i class="fab fa-facebook"></i></a>
-        <a href="https://instagram.com/gith.amd_" target="_blank"><i class="fab fa-instagram"></i></a>
-        <a href="https://twitter.com/githriff" target="_blank"><i class="fab fa-twitter"></i></a>
-        <a href="https://t.me/githriff" target="_blank"><i class="fab fa-telegram"></i></a>
-    </div>
-
-    <p class="copyright">&#169; 2023 All Rights reserved. This site is made with kang love by <a href="https://instagram.com/gith.amd_" target="_blank">@gith.amd_</a></p>
-
 
 </body>
 </html>
